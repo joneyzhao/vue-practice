df1045c611683429ed42ce8a2e9e53b260d274f5
humenglin <44660775+humenglin@users.noreply.github.com>
2019-03-05 16:25:37 +0800

    Update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -4,18 +4,22 @@ pipeline {
 	stages {
 		stage('代码扫描') {
 			steps {
+				echo "代码扫描"
 			}
 		}
 		stage('单元测试') {
 			steps {
+				echo "单元测试"
 			}
 		}
 		stage('打包构建') {
 			steps {
+				echo "打包构建"
 			}
 		}
 		stage('部署') {
 			steps {
+				echo "部署"
 			}
 		}
 	}
