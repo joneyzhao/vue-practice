b080e4aed23eaaad5b9ff6ac9b5db11ba16a0d8a
humenglin <44660775+humenglin@users.noreply.github.com>
2019-03-05 15:39:25 +0800

    update Jenkinsfile.groovy

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -3,12 +3,20 @@ pipeline {
 	
 	stages {
 		stage('代码扫描') {
+			steps {
+			}
 		}
 		stage('单元测试') {
+			steps {
+			}
 		}
 		stage('打包构建') {
+			steps {
+			}
 		}
 		stage('部署') {
+			steps {
+			}
 		}
 	}
 }
