2f7caf839f87eaca55d2e314752d4c459d0064b8
BIBOBIBO <fxw1988@126.com>
2019-03-06 17:55:10 +0800

    将测试和打包换成真正的命令

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -16,11 +16,13 @@ pipeline {
 		stage("单元测试") {
 			steps {
 				echo "单元测试"
+				sh "npm run test:unit"
 			}
 		}
 		stage("打包构建") {
 			steps {
 				echo "打包构建"
+				sh "npm run build"
 			}
 		}
 		stage("部署") {
